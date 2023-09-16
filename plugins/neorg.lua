9f2366a968bfe165dfa1f4c35e004265e306572e
Arismendy Evangelista <ArismendyEZ115@hotmail.com>
2023-09-16 18:04:45 -0400

    Adding neorg

---
+++ plugins/neorg.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    run = ":Neorg sync-parsers",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                work = "F:/notes/work",
+                personal = "F:/notes/personal"
+              }
+            }
+          }
+        }
+      }
+    end,
+    cmd = "Neorg"
+  }
+}
